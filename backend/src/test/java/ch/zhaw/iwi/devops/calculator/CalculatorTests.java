ef7e85ff6700dac6a231706b88140d10bd22b47d
timonschneider <timon.schneider@hotmail.com>
2023-06-04 22:18:21 +0200

    Test Coverage addition

--- backend/src/test/java/ch/zhaw/iwi/devops/calculator/CalculatorTests.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/calculator/CalculatorTests.java
@@ -20,6 +20,21 @@ public class CalculatorTests {
         assertEquals(-1, calculator.subtract(2, 3));
     }
 
- 
+    @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+    @Test
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        assertEquals(2.0, calculator.divide(6, 3), 0.001);
+    }
+
+    @Test
+    public void testCircle() {
+        Calculator calculator = new Calculator();
+        assertEquals(18.85, calculator.calculateCircle(3), 0.001);
+    }
 
 }
