c741cfbf5b3e0130876b25f1a4a7d7e7830ca51e
timonschneider <timon.schneider@hotmail.com>
2023-06-04 09:14:24 +0200

    Fixed Testing FizzBuzz

--- backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
@@ -26,7 +26,7 @@ public class FizzBuzzConverterTest {
 
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
-        Assertions.assertEquals("Buzz", fizzBuzz.convert(3));
+        Assertions.assertEquals("Fizz", fizzBuzz.convert(3));
     }
     
     @Test
